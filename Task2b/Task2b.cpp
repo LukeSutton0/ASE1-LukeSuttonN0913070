61aed2774865fec28f46db49edae1cd689ce9b25
Luke <80263461+LukeSutton0@users.noreply.github.com>
2022-12-13 19:55:00 +0000

    functions made, task2b acting bad

--- Task2b/Task2b.cpp
+++ Task2b/Task2b.cpp
@@ -1,24 +1,42 @@
 #include "../headers/Task2.h"
-#include <list>
-#include <map>
-#include <sstream>
-
-
-int task2b(std::string filePath) {
-    std::ifstream testFile {filePath};
-    std::string line;
-    std::list<std::string> bricks;
-
-    if (!testFile.is_open()) { //if not open
-        std::cerr << "File not open" << "\n"; //output char error
+int main(int argc, char* argv[]) {   
+    //std::string filePath = checkIfInput(argc, argv);
+    std::string filePath = "input-pairs-50.txt";
+    if (filePath == "Error invalid File") {
         return 1;
     }
+    std::list<std::string> bricks;
+    std::string startBrick = fileToList(filePath, bricks);
+    if (startBrick == "Error") {
+        return 1;
+    }
+    std::map<std::string, std::string> mapForSearching;
+    listToOMap(bricks,mapForSearching);
+    std::list<std::string> result;
+    std::string nextBrick = startBrick;
+    result.push_back(nextBrick);
+    twoBSouth(bricks,mapForSearching,result,nextBrick);
+    std::string lastBrick = "";
+    twoBNorth(bricks, mapForSearching, result, nextBrick, lastBrick);
+    
+    for (const auto& resultIter : result) {
+        std::cout << resultIter;
+    }
+    return 0;
+}
 
-    while (std::getline(testFile,line)) {
+std::string fileToList(std::string& filePath, std::list<std::string>& bricks) {
+    std::ifstream testFile{ filePath };
+    std::string line;
+    if (!testFile.is_open()) { //if not open
+        std::cerr << "File not open" << "\n"; //output char error
+        return "Error";
+    }
+    while (std::getline(testFile, line)) {
         //split string
         std::string delimiter = ",";
         std::string firstString = line.substr(0, line.find(delimiter));
-        std::string secondString = line.substr(line.find(delimiter)+1,line.back());
+        std::string secondString = line.substr(line.find(delimiter) + 1, line.back());
         bricks.push_back(firstString);
         bricks.push_back(secondString);
     }
@@ -26,13 +44,13 @@ int task2b(std::string filePath) {
     testFile.seekg(0, testFile.beg);
     std::string startBrick;
     std::getline(testFile, startBrick, ','); //get string from line 1 / can change to whatever if need
-
-    std::map<std::string,std::string> mapForSearching;
+    return startBrick;
+}
+void listToOMap(std::list<std::string>& bricks, std::map<std::string, std::string>& mapForSearching) {
     int countingListEntries = 0;
     std::string mapValue1 = " ";
-
     for (std::list<std::string>::const_iterator itToListCurrentBrick = bricks.begin(); itToListCurrentBrick != bricks.end(); ++itToListCurrentBrick) { //add list contents to map
-        if(countingListEntries %2 == 1){
+        if (countingListEntries % 2 == 1) {
             std::string mapValue2 = *itToListCurrentBrick;
             mapForSearching[mapValue1] = mapValue2;
         }
@@ -41,12 +59,9 @@ int task2b(std::string filePath) {
         }
         ++countingListEntries;
     }
-    std::list<std::string> result;
-    bool finishedNorth = false;
+}
+void twoBSouth(std::list<std::string>& bricks, std::map<std::string, std::string>& mapForSearching, std::list<std::string>& result, std::string nextBrick) {
     bool finishedSouth = false;
-    std::string nextBrick = startBrick;
-    std::string lastBrick = "";
-    
     while (!finishedSouth) {
         if (mapForSearching.find(nextBrick) != mapForSearching.end()) {
             result.push_back("\n" + mapForSearching[nextBrick]);
@@ -56,47 +71,41 @@ int task2b(std::string filePath) {
             finishedSouth = true;
         }
     }
-    nextBrick = startBrick;
-    result.push_front(nextBrick);
+}
+void twoBNorth(std::list<std::string>& bricks, std::map<std::string, std::string>& mapForSearching, std::list<std::string>& result, std::string nextBrick, std::string lastBrick) {
+    bool finishedNorth = false;
     while (!finishedNorth) {
         if (mapForSearching.find(nextBrick) != mapForSearching.end() && mapForSearching.find(nextBrick)->second == nextBrick) { //tried using string but took ages to push front
             result.push_front(nextBrick + "\n");
             nextBrick = mapForSearching[nextBrick];
-        }        
+        }
         else {
             finishedNorth = true;
         }
     }
-    for (const auto& resultIter : result) {
-        std::cout << resultIter;
-    }
-    /*std::stringstream stream(result);
-    std::string resultString;
-    /*while (std::getline(stream, resultString, ',')) {
-        std::cout << resultString <<"\n";
-    }*/
-
-    return 0;
 }
-
-int main(int argc, char* argv[]) {
-    /*std::string filePath = "input-pairs-3M.txt";
-    task2b(filePath);*/
-    if (argc != 1) { //make sure filepath entered
-        try {
+std::string checkIfInput(int argc, char* argv[]) {
+    try {
+        if (argc != 1) { //make sure filepath entered
             std::string filePath = argv[1];
-            task2b(filePath);
-        }
-        catch(...) {
-            std::cerr << "Error establishing filepath";
+            return filePath;
         }
     }
-    //task2b("input-pairs-1K.txt");
+    catch (...) {
+        std::cerr << "Error establishing filepath";
+    }
+    return "Error invalid File";
+
 }
 
 
 //code graveyard
 //circle star square rectangle diamond octagon
-    //task2b("input-pairs-1K.txt");
-    //result = nextBrick + result;
-            //result = result + "," + mapForSearching[nextBrick];
+//    task2b("input-pairs-1K.txt");
+//    result = nextBrick + result;
+//            result = result + "," + mapForSearching[nextBrick];
+//std::stringstream stream(result);
+//    std::string resultString;
+//    while (std::getline(stream, resultString, ',')) {
+//        std::cout << resultString <<"\n";
+//    }
