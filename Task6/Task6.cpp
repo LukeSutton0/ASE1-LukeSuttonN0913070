0477c2322dae28960e54293e081c05d36c2cec8e
Luke <80263461+LukeSutton0@users.noreply.github.com>
2022-12-15 22:46:02 +0000

    task 6 done

--- Task6/Task6.cpp
+++ Task6/Task6.cpp
@@ -17,15 +17,17 @@ std::string checkIfInput(int argc, char* argv[]) {
 
 }
 
-void addToTape(std::string& filePath, std::list<std::pair<std::string, std::string>>&tapeA, std::list<std::pair<std::string, std::string>>& tapeB) {
+int addToTape(std::string& filePath, std::list<std::pair<std::string, std::string>>&tapeA, std::list<std::pair<std::string, std::string>>& tapeB) {
     std::ifstream testFile{ filePath };
     std::string line;
+    int N = 0;
     if (!testFile.is_open()) { //if not open
         std::cerr << "File not open" << "\n"; //output char error
     }
     else {
         while (std::getline(testFile, line)) {
             //split string
+            N = N + 2;
             std::string delimiter = ",";
             std::string firstString = line.substr(0, line.find(delimiter));
             std::string secondString = line.substr(line.find(delimiter) + 1, line.back());
@@ -34,12 +36,124 @@ void addToTape(std::string& filePath, std::list<std::pair<std::string, std::stri
             tapeB.push_back(pair);
         }
     }
+    return N;
 }
 
-void sortTapeA(std::list<std::pair<std::string, std::string>>& tapeA) {
-    tapeA.sort();
+void sortTapeSecond(std::list<std::pair<std::string, std::string>>& tape) {
+    tape.sort([](const std::pair<std::string, std::string>& pair1,const std::pair<std::string, std::string>& pair2) {  //lambda function
+            return pair1.second < pair2.second; //a = smallest char  Z= biggest
+        }
+    );
+        
+}
+void sortTapeSecond(std::list<std::pair<std::string, int>>& tape) {
+    tape.sort([](const std::pair<std::string, int>& pair1, const std::pair<std::string, int>& pair2) {  //lambda function
+        return pair1.second < pair2.second; //a = smallest char  Z= biggest
+        }
+    );
+
 }
 
+void sortTapeFirst(std::list<std::pair<std::string, std::string>>& tape) {
+    tape.sort([](const std::pair<std::string, std::string>& pair1, const std::pair<std::string, std::string>& pair2) {  //lambda function
+        return pair1.first < pair2.first; //a = smallest char  Z= biggest
+        }
+    );
+}
+
+void sortTapeFirst(std::list<std::pair<std::string, int>>& tape) { //overload for string int pair
+    tape.sort([](const std::pair<std::string,int>& pair1, const std::pair<std::string,int>& pair2) {  //lambda function
+        return pair1.first < pair2.first; //a = smallest char  Z= biggest
+        }
+    );
+}
+
+
+
+void compareAB(std::list<std::pair<std::string, std::string>>& tapeA, std::list<std::pair<std::string, std::string>>& tapeB,
+    std::list<std::pair<std::string, std::string>>& tapeC, std::list<std::pair<std::string, int>>& tapeP) {
+
+    auto tapeAit = tapeA.begin();
+    auto tapeBit = tapeB.begin();
+    while(tapeAit != tapeA.end()){
+        if (tapeAit->second == tapeBit->first) {
+            auto pair = std::make_pair(tapeAit->first, tapeBit->second);
+            tapeC.push_back(pair);
+            tapeAit++;
+            tapeBit++;
+        }
+        else if (tapeAit->second < tapeBit->first) {
+            int pos = std::distance(tapeA.begin(), tapeAit);
+            auto pair = std::make_pair(tapeAit->first, (pos - 1));
+            auto pair2 = std::make_pair(tapeAit->second,pos);
+            tapeP.push_back(pair);
+            tapeP.push_back(pair2);
+            tapeAit++;
+        }
+        else if (tapeAit->second > tapeBit->first) {
+            tapeBit++;
+        }
+        else {
+            std::cout << "error";
+        }
+        
+    }
+    sortTapeFirst(tapeP);
+}
+
+
+void tapeManipulation(std::list<std::pair<std::string, std::string>>& tapeA, std::list<std::pair<std::string, std::string>>& tapeB,
+    std::list<std::pair<std::string, std::string>>& tapeC, std::list<std::pair<std::string, int>>& tapeP, std::list<std::pair<std::string,int>>& tapeF,int& d) {
+    auto tapeAit = tapeA.begin();
+    auto tapeBit = tapeB.begin();
+    auto tapePit = tapeP.begin();
+
+    bool tapePEnd = false;
+    bool tapeBEnd = false;
+    while (tapeAit != tapeA.end()) {
+        if (tapePit == tapeP.end()) {
+            tapePit--;
+            tapePEnd = true;
+        }
+        if (tapeBit == tapeB.end()) {
+            tapeBit--;
+            tapeBEnd = true;
+        }
+        if (tapeAit->second == tapeBit->first /*&& tapeBEnd == false*/) {
+            auto pair = std::make_pair(tapeAit->first,tapeBit->second);
+            tapeC.push_back(pair);
+            tapeAit++;
+            tapeBit++;
+        }
+        else if (tapeAit->second == tapePit->first /*&& tapePEnd == false*/) {
+            auto pair = std::make_pair(tapeAit->first, tapePit->second - d);
+            tapeF.push_back(pair);
+            tapeAit++;
+            tapePit++;
+        }
+        else if (tapeAit->second > tapePit->first && tapePEnd == false) {
+            tapePit++;
+        }
+        else if (tapeAit->second > tapeBit->first && tapeBEnd == false) {
+            tapeBit++;
+        }
+        else {
+            tapeAit++;
+        }
+    }
+}
+
+void mergeTapes(std::list<std::pair<std::string, int>>& tapeP, std::list<std::pair<std::string, int>>& tapeF) { //f into p preserving alphabetical ordering of first components);
+    auto tapePit = tapeP.begin();
+    auto tapeFit = tapeF.begin();
+    tapeP.merge(tapeF);
+}
+
+void copyToResult(std::list<std::pair<std::string, int>>& tapeP, std::list<std::string> tapeResult) {
+    for (auto pair : tapeP) {
+        tapeResult.push_back(pair.first);
+    }
+}
 
 
 
@@ -51,12 +165,46 @@ int main(int argc, char* argv[]) {
     }
     std::list<std::pair<std::string, std::string>> tapeA;
     std::list<std::pair<std::string, std::string>> tapeB;
-    addToTape(filePath, tapeA,tapeB);
-    sortTapeA(tapeA);
+    std::list<std::pair<std::string, std::string>> tapeC;
+    std::list<std::pair<std::string, int>> tapeP;
+    int N = addToTape(filePath, tapeA,tapeB);
+    sortTapeSecond(tapeA);
+    sortTapeFirst(tapeB);
+    compareAB(tapeA, tapeB,tapeC,tapeP);
+
+    int d = 2;
+    std::list<std::pair<std::string, int>> tapeF;
 
 
-    for (auto line : tapeA) {
-        std::cout << line.first << " "<< line.second << "\n";
+
+
+    while (d < N) {
+
+        tapeA = tapeC;
+        tapeB = tapeC;
+        tapeC.clear();
+        sortTapeSecond(tapeA);
+        sortTapeFirst(tapeB);
+        tapeManipulation(tapeA,tapeB,tapeC,tapeP,tapeF,d);
+        sortTapeFirst(tapeF);
+        //mergeTapes(tapeP,tapeF);
+        tapeP.merge(tapeF);
+        tapeF.clear();
+        d = d * 2;
     }
+
+    sortTapeSecond(tapeP);
+
+    std::list<std::string> tapeResult;
+    for (auto pair : tapeP) {
+        tapeResult.push_back(pair.first);
+    }
+
+    for (auto brick : tapeResult) {
+        std::cout << brick << "\n";
+    }
+
+
+
 
 }
